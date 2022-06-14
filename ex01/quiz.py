adc8fa16bc12b2464428c26f18d971ec0b740d7e
Shiryu <c0a2112210@edu.teu.ac.jp>
2022-06-14 15:51:42 +0900

    初めてのぱいそん

---
+++ ex01/quiz.py
@@ -0,0 +1,38 @@
+import random
+
+x = (random.randint(1,4))
+
+#一問目 
+if (x == 1):
+    print("問題：\nサザエの旦那の名前は？")
+    answer = input("答えを入力してください：")
+    if (answer == "マスオ"):
+        print("正解です！")
+    elif (answer == "ますお"):
+        print("正解です！")
+    else:
+        print("不正解です")
+#二問目 
+if (x == 2):
+    print("問題：\nカツオの妹の名前は？")
+    answer = input("答えを入力してください：")
+    if (answer == "ワカメ"):
+        print("正解です！")
+    elif (answer == "わかめ"):
+        print("正解です！")
+    else:
+        print("不正解です")
+#三問目 
+if (x == 3):
+    print("問題：\nタラオはカツオから見てどんな関係？")
+    answer = input("答えを入力してください：")
+    if (answer == "甥"):
+        print("正解です！")
+    elif (answer == "おい"):
+        print("正解です！")
+    elif (answer == "甥っ子"):
+        print("正解です！")
+    elif (answer == "おいっこ"):
+        print("正解です！")
+    else:
+        print("不正解です")
