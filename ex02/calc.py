7ff68e994e57717488f7d2eff84f2d16635f3ac4
Shiryu <c0a2112210@edu.teu.ac.jp>
2022-06-21 15:25:53 +0900

    6問目

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,12 +1,21 @@
 
 import tkinter as tk
 import tkinter.messagebox as tkm
+from weakref import ReferenceType
 
+#ボタンの処理
 def button_click(event):
     btn = event.widget
     txt = btn["text"]
     #tkm.showinfo(txt, f"[{txt}]ボタンが押されました")
-    entry.insert(tk.END,txt)
+    if txt == "=":
+        result = entry.get()
+        entry.eval(result)
+        entry.delete(0,tk.END)
+        entry.insert(tk.END,txt)
+
+    else:
+        entry.insert(tk.END,txt)
 
 #Tkクラス
 if __name__ == "__main__":
@@ -18,7 +27,7 @@ if __name__ == "__main__":
     #ボタンクラス
     r, c = 1, 0
     for i in range(9, -1, -1):
-        button = tk.Button(root,
+        button = tk.Button(root,        #1~9までのボタンを作成
                             text = f"{i}",
                             font = ("Times New Roman", 30),
                             width=4, height=2,
@@ -26,9 +35,17 @@ if __name__ == "__main__":
         button.bind("<1>",button_click)
         button.grid(row = r, column= c) #rowをrに、columnをcに
         c+=1
-        if (i - 1) % 3 == 0:
+        if (i - 1) % 3 == 0:            #ボタンの配列
             r += 1
             c = 0
+    button = tk.Button(root, text="+", font=("Times New Roman",30), width=4, height=2)  #"+"ボタンの作成
+    button.grid(row=r, column=c)        #空いた位置に作成
+    button.bind("<1>",buttton_plus)     #"+"のクリック処理
+    button = tk.Button(root, text="=", font=("Times New Roman",30), width=4, height=2)  #"="ボタンの作成
+    button.grid(row=r, column=c+1)      #"+"の隣に作成
+    button.bind("<1>",button_result)    #"="のクリック処理
+
+
 
     #Entryクラス
     entry = tk.Entry(root,justify="right", width="10", font=("Times New Roman",40))
