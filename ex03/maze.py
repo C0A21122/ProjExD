42ce8b2344f46ed87153eef5157e8781cb123876
Shiryu <c0a2112210@edu.teu.ac.jp>
2022-06-28 14:12:08 +0900

    2問目

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,9 +1,14 @@
 import tkinter
+from turtle import bgcolor
 
 
 
 if __name__ == "__main__":
     root = tkinter.Tk()
     root.title("迷えるこうかとん")
+    
+
+    canvas = tkinter.Canvas(root, width=1500, height=900, bg="BLACK")
+    canvas.pack()
 
     root.mainloop()
