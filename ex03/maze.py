bf8d40f9e070a91b030dc38b2fb98528f866d719
Shiryu <c0a2112210@edu.teu.ac.jp>
2022-06-28 14:23:31 +0900

    6問目

--- ex03/maze.py
+++ ex03/maze.py
@@ -4,6 +4,10 @@ def key_down(event):
     global key
     key = event.keysym
 
+def key_up():
+    global key
+    key = ""
+
 if __name__ == "__main__":
     global key
     key = ""
@@ -28,5 +32,6 @@ if __name__ == "__main__":
 
     #bindクラス
     root.bind("<KeyPress>", key_down)
+    root.bind("<KeyRelease>", key_up)
 
     root.mainloop()
