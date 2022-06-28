c9ad8b14365d4756ebee23e0bd768e476e567b0f
Shiryu <c0a2112210@edu.teu.ac.jp>
2022-06-28 14:18:07 +0900

    3問目

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,14 +1,23 @@
 import tkinter
-from turtle import bgcolor
-
-
 
 if __name__ == "__main__":
     root = tkinter.Tk()
     root.title("迷えるこうかとん")
     
-
-    canvas = tkinter.Canvas(root, width=1500, height=900, bg="BLACK")
+    #Canvasクラス
+    canvas = tkinter.Canvas(root, 
+                            width=1500, #幅
+                            height=900, #高さ
+                            bg="BLACK"
+                            ) #背景色
+    
+    #PhotoImageクラス
+    tori = tkinter.PhotoImage(file = "ex03/fig/3.png")
+    cx, cy = 300, 400
+    canvas.create_image(cx, #x座標
+                        cy, #y座標
+                        image=tori #画像
+                        )
     canvas.pack()
 
     root.mainloop()
