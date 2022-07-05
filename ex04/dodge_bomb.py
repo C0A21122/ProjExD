ec35591beb9bde7e60e3eeca3149aa8264ee8e43
Shiryu <c0a2112210@edu.teu.ac.jp>
2022-07-05 14:24:08 +0900

    3

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -3,20 +3,29 @@ import sys
 
 def main():
     pygame.display.set_caption("逃げろ！こうかとん")    #ウィンドウの名前
-    screen = pygame.display.set_mode((1600,900))       #ウィンドウサイズ
+    screen = pygame.display.set_mode((1600,900))       #ウィンドウ
+    clock = pygame.time.Clock()
     screen_rect = screen.get_rect()
-    bg_iamge = pygame.image.load("ex04/fig/pg_bg.jpg")  #背景
+
+    #背景
+    bg_iamge = pygame.image.load("ex04/fig/pg_bg.jpg")  
     bg_rect = bg_iamge.get_rect()
     screen.blit(bg_iamge, bg_rect)
 
-    clock = pygame.time.Clock()
-    clock.tick(0.5)
+    #こうかとん
+    tori_img = pygame.image.load("ex03/fig/2.png")      
+    tori_img = pygame.transform.rotozoom(tori_img,0, 2.0)
+    tori_rect = tori_img.get_rect()
+    tori_rect.center = 900, 400
 
     while True:
-        screen.blit(bg_iamge, bg_rect)
+        screen.blit(bg_iamge, bg_rect)  #背景の貼り付け
+        screen.blit(tori_img, tori_rect)#こうかとんの貼り付け
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
         pygame.display.update()
         clock.tick(1000)
 
